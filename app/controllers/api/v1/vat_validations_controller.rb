954951b92dc8e1cdb15072a8e70fc8ee6a18ece6
sofi fuhrmann <sofia.fuhrmann@aerolaplata.com.ar>
2026-05-15 20:14:39 -0300

    Add VAT validations API structure
    - Add initial VAT validation API endpoints.
    - Create controller and routes structure.
    - Add Bruno endpoints for future API testing.

---
+++ app/controllers/api/v1/vat_validations_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class VatValidationsController < ApplicationController
+      def create
+      end
+
+      def show
+      end
+
+      def index
+      end
+
+      def stats
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :vat_validations, only: %i[create show index] do
+        get :stats, on: :collection
+      end
+    end
+  end
 end
